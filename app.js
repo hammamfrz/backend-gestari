683aec2ce737eaaaff1f9a7ab9909203dba49ecd
hammam06 <hammam.raharjo@gmail.com>
2022-12-04 18:47:16 +0700

    update cors

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const transactionRouter = require('./app/transaction/route')
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
