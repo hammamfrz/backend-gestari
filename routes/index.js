f54b6439c05fab5b3f549c5bab09dcdd982e6778
hammam06 <hammam.raharjo@gmail.com>
2022-12-04 18:27:24 +0700

    update cors

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+
+const cors = require('cors');
+
+app.use(cors());
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Gestari' });
 });
 
 module.exports = router;
