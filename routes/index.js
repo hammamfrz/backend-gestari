139b0f7200a6b0afdabd2af84bc193e096968598
hammam06 <hammam.raharjo@gmail.com>
2022-12-04 19:31:13 +0700

    update cors

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,9 @@ var express = require('express');
 var router = express.Router();
 var app = express();
 
-const cors = require('cors');
-
-app.use(cors({
-  origin: "*",
-}));
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
-
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
 });
 
 module.exports = router;
