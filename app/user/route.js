321bdd0fde58418c887b07ddce027d281ddd8b36
hammam06 <hammam.raharjo@gmail.com>
2022-12-06 17:58:48 +0700

    hampir selesai

--- app/user/route.js
+++ app/user/route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { handlerGetUser, handlerCreateUser, handlerUpdateUser, handlerDeleteUser, handlerLoginUser, handlerLogoutUser } = require('./handler');
+const { handlerGetUser, handlerCreateUser, handlerUpdateUser, handlerDeleteUser, handlerLoginUser, handlerLogoutUser, handlerUpdateUserbyAdmin } = require('./handler');
 const router = express.Router();
 
 // API 1: GET /user
@@ -19,6 +19,9 @@ router.post('/login', handlerLoginUser);
 
 // API 6: LOGOUT /user/logout
 router.post('/logout', handlerLogoutUser);
+
+// API 7: UPDATE /user/admin/:id
+router.put('/admin/:id', handlerUpdateUserbyAdmin);
 
 
 
