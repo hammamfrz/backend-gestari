8120aa7fcf3cc3e17346dd3cf5749a93f92379a6
hammam06 <hammam.raharjo@gmail.com>
2022-12-08 18:16:49 +0700

    resolve katalog

--- validator/katalog/schema.js
+++ validator/katalog/schema.js
@@ -2,18 +2,16 @@ const joi = require('joi');
 
 const katalogCreateSchema = joi.object({
     name: joi.string().min(3).max(30).required(),
-    description: joi.string().min(3).max(30).required(),
     price: joi.number().required(),
     type: joi.string().min(3).max(30).required(),
-    image: joi.string().min(3).max(30).required(),
+    image: joi.string().min(3).max(100).required(),
 }).unknown();
 
 const katalogUpdateSchema = joi.object({
     name: joi.string().min(3).max(30).required(),
-    description: joi.string().min(3).max(30).required(),
     price: joi.number().required(),
     type: joi.string().min(3).max(30).required(),
-    image: joi.string().min(3).max(30).required(),
+    image: joi.string().min(3).max(100).required(),
 }).unknown();
 
 module.exports = {
